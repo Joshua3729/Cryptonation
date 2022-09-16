2e912ce28488224d4ffc1005b7b1e4458fcc1914
Joshua <3729344@myuwc.ac.za>
2022-09-16 17:20:23 +0200

    added Browser-Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Coin_Details from "./Pages/Coin_Details_Page/Coin_Details_Page";
 import Landing_Page from "./Pages/Landing_Page/Landing_Page";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   // return <Landing_Page />;
-  return <Coin_Details />;
+  return (
+    <Routes>
+      <Route path="/" exact element={<Landing_Page />} />
+      <Route path="/coin/:coin_id" exact element={<Coin_Details />} />
+    </Routes>
+  );
 }
 
 export default App;
