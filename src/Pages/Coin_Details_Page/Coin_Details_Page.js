91f7cea75ded844cb04ef7e3118a020d39794092
Joshua <3729344@myuwc.ac.za>
2022-09-17 12:40:02 +0200

    added market stat section

--- src/Pages/Coin_Details_Page/Coin_Details_Page.js
+++ src/Pages/Coin_Details_Page/Coin_Details_Page.js
@@ -11,9 +11,6 @@ const Coin_Details = () => {
   const [active_tab, set_active_tab] = useState("price");
   const coin_id = useParams().coin_id;
 
-  const url =
-    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=false";
-
   useEffect(() => {
     const currDate = new Date();
     const prevDate = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
@@ -22,7 +19,9 @@ const Coin_Details = () => {
     const prevTimeStamp = Math.floor(prevDate.getTime() / 1000);
 
     axios
-      .get(url)
+      .get(
+        `https://api.coingecko.com/api/v3/coins/${coin_id}?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=false`
+      )
       .then((crypto_coin) => get_coin_details(crypto_coin.data))
       .catch((error) => console.log(error));
 
@@ -42,7 +41,7 @@ const Coin_Details = () => {
   console.log(chart_data);
 
   let coinDetails = <p>Loading...</p>;
-  if (chart_data && coin_details)
+  if (chart_data && coin_details) {
     switch (active_tab) {
       case "price":
         chartData = chart_data.prices;
@@ -65,81 +64,110 @@ const Coin_Details = () => {
 
         break;
     }
-  coinDetails = (
-    <div className={classes.Coin_Details}>
-      <div className={classes.Coin_Details_wrapper}>
-        <div className={classes.header_bar_wrapper}>
-          <div className={classes.coin_icon}>
-            <img src={coin_details?.image.small} alt="coin_icon" />
-          </div>
-          <div className={classes.coin_identifier}>
-            <div className={classes.coin_name_wrapper}>
-              <div className={classes.coin_name}>Bitcoin</div>
-              <div className={classes.coin_symbol}>BTC</div>
+    coinDetails = (
+      <div className={classes.Coin_Details}>
+        <div className={classes.Coin_Details_wrapper}>
+          <div className={classes.header_bar_wrapper}>
+            <div className={classes.coin_icon}>
+              <img src={coin_details.image.small} alt="coin_icon" />
             </div>
-            <div className={classes.percentage_tracker}>
-              +ZAR(
-              <span
-                className={
-                  coin_details?.market_data.market_cap_change_percentage_24h > 0
-                    ? classes.increase
-                    : classes.decrease
-                }
-              >
-                {coin_details?.market_data.price_change_percentage_24h.toFixed(
-                  2
-                )}
-                %
-              </span>
-              )
-            </div>
-          </div>
-        </div>
-        <div className={classes.chart_wrapper}>
-          <div className={classes.toggle_btns_wrapper}>
-            <div className={classes.toggle_btns_innerWrapper}>
-              <button
-                className={
-                  active_tab == "price"
-                    ? [classes.btn, classes.active].join(" ")
-                    : classes.btn
-                }
-                onClick={() => set_active_tab_handler("price")}
-              >
-                Price
-              </button>
-              <button
-                className={
-                  active_tab == "market_cap"
-                    ? [classes.btn, classes.active].join(" ")
-                    : classes.btn
-                }
-                onClick={() => set_active_tab_handler("market_cap")}
-              >
-                Market cap
-              </button>
-              <button
-                className={
-                  active_tab == "total_volumes"
-                    ? [classes.btn, classes.active].join(" ")
-                    : classes.btn
-                }
-                onClick={() => set_active_tab_handler("total_volumes")}
-              >
-                total_Volume
-              </button>
+            <div className={classes.coin_identifier}>
+              <div className={classes.coin_name_wrapper}>
+                <div className={classes.coin_name}>{coin_details.name}</div>
+                <div className={classes.coin_symbol}>{coin_details.symbol}</div>
+              </div>
+              <div className={classes.percentage_tracker}>
+                +ZAR(
+                <span
+                  className={
+                    coin_details.market_data.market_cap_change_percentage_24h >
+                    0
+                      ? classes.increase
+                      : classes.decrease
+                  }
+                >
+                  {coin_details.market_data.price_change_percentage_24h.toFixed(
+                    2
+                  )}
+                  %
+                </span>
+                )
+              </div>
             </div>
           </div>
-          <Chart_component
-            chart_data={chartData || []}
-            increasing={increasing}
-            lineWidth={1.5}
-            chart_height={"400px"}
-          />
+          <div className={classes.chart_wrapper}>
+            <div className={classes.toggle_btns_wrapper}>
+              <div className={classes.toggle_btns_innerWrapper}>
+                <button
+                  className={
+                    active_tab == "price"
+                      ? [classes.btn, classes.active].join(" ")
+                      : classes.btn
+                  }
+                  onClick={() => set_active_tab_handler("price")}
+                >
+                  Price
+                </button>
+                <button
+                  className={
+                    active_tab == "market_cap"
+                      ? [classes.btn, classes.active].join(" ")
+                      : classes.btn
+                  }
+                  onClick={() => set_active_tab_handler("market_cap")}
+                >
+                  Market cap
+                </button>
+                <button
+                  className={
+                    active_tab == "total_volumes"
+                      ? [classes.btn, classes.active].join(" ")
+                      : classes.btn
+                  }
+                  onClick={() => set_active_tab_handler("total_volumes")}
+                >
+                  total_Volume
+                </button>
+              </div>
+            </div>
+            <Chart_component
+              chart_data={chartData || []}
+              increasing={increasing}
+              lineWidth={1.5}
+              chart_height={"400px"}
+            />
+          </div>
+          <div className={classes.market_stats_wrapper}>
+            <h2 className={classes.secondary_header}>
+              Market stats and details
+            </h2>
+            <div className={classes.market_stats_innerWrapper}>
+              <div className={classes.market_stat_item}>
+                <p className={classes.header}>All time high</p>
+                <p className={classes.stat}>R300</p>
+              </div>
+              <div className={classes.market_stat_item}></div>
+              <div className={classes.market_stat_item}></div>
+              <div className={classes.market_stat_item}></div>
+              <div className={classes.market_stat_item}></div>
+              <div className={classes.market_stat_item}></div>
+              <div className={classes.market_stat_item}></div>
+              <div className={classes.market_stat_item}></div>
+            </div>
+          </div>
+          <div className={classes.about_wrapper}>
+            <h2 className={classes.secondary_header}>
+              About {coin_details.name}
+            </h2>
+            <div
+              className={classes.about_text}
+              dangerouslySetInnerHTML={{ __html: coin_details.description.en }}
+            ></div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 
   return (
     <>
