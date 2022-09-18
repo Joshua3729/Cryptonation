0527d438a024aca27575af6bacd2245ee71d7593
Joshua <3729344@myuwc.ac.za>
2022-09-18 23:39:31 +0200

    fixing a production routing issue

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" exact element={<Landing_Page />} />
-      <Route path="/coin/:coin_id" exact element={<Coin_Details />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/" element={<Landing_Page />} />
+      <Route path="coin" element={<Coin_Details />} />
+      <Route path="coin/:coin_id" element={<Coin_Details />} />
     </Routes>
   );
 }
