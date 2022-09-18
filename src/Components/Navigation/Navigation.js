8325bb043a0d85b71f00fbf4e4397fb76a26afe3
Joshua <3729344@myuwc.ac.za>
2022-09-18 21:51:22 +0200

    added logo link

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BurgerButton from "../UI/BurgerButton/BurgerButton";
 import classes from "./Navigation.module.css";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div className={classes.Navigation}>
-      <div className={classes.logo}>CRYPTO-JOINT</div>
+      <div className={classes.logo}>
+        <Link to="/">CRYPTO-JOINT</Link>
+      </div>
       <ul className={classes.nav_list}>
         <li className={classes.nav_list_item}>Support</li>
         <li className={classes.nav_list_item}>About</li>
